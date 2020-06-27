19f8a670e59ba22f01e6f06add1b3fcc720bf92d
spncr <spencerwberry@gmail.com>
2020-06-27 15:20:37 -0700

    Refactored into simpler classes and organized Monolithic code into sections

--- inkgrid.js
+++ inkgrid.js
@@ -1,14 +1,14 @@
 const BOARD_SIZE = 13;
 
-let gridSize;
-let halfGridSize;
-let quarterGridSize;
-let eighthGridSize;
-let sixteenthGridSize;
+// let gridSize;
+// let halfGridSize;
+// let quarterGridSize;
+// let eighthGridSize;
+// let sixteenthGridSize;
 let boardCanvas;
 let currentMatch;
+let p1
 
-let p1;
 
 function setup() {
   defineGridSize();
@@ -16,79 +16,95 @@ function setup() {
   boardCanvas.mousePressed(mouseDown);
   boardCanvas.mouseReleased(mouseUp);
   currentMatch = new Match();
+
   palette = {
     background: 0,
     neutral: 30,
     playerOne: color(113, 5, 136),
     playerTwo: color(37, 105, 255)
   }
-  p1 = new Player({row: Math.floor(Math.random()*BOARD_SIZE), column: BOARD_SIZE-1}, 'One', palette.playerOne);
+
+  p1 = new Player({column: Math.floor(Math.random()*BOARD_SIZE), row: BOARD_SIZE-1}, 'One', palette.playerOne);
 }
 
 function draw(){
   background(palette.background);
   drawGrid();
   drawOutline();
-  //p1.draw();
   currentMatch.update();
+  currentMatch.draw();
+  p1.draw();
 }
 
+////// CELL \\\\\\
 class Cell {
-  constructor(column, row, color){
+  constructor(column, row, color) {
     this.column = column;
     this.row = row;
     this.color = color;
    }
 }
 
-var Player = function(cell, name, color){
-  this.cell = cell;
-  this.name = name;
-  this.color = color;
-  this.territory = [];
-}
-
-Player.prototype.draw = function(){
-  let top_left_coord = { x: this.cell.column * gridSize, y: this.cell.row * gridSize};
-  strokeWeight(sixteenthGridSize);
-  stroke(palette.neutral);
-  fill(this.color);
-  quad(top_left_coord.x + halfGridSize, top_left_coord.y,
-    top_left_coord.x + gridSize, top_left_coord.y + halfGridSize,
-    top_left_coord.x + halfGridSize, top_left_coord.y + gridSize,
-    top_left_coord.x, top_left_coord.y + halfGridSize);
-  text(this.name,top_left_coord.x, top_left_coord.y);
-}
-
-var Match = function() {
-  this.players = ["playerOne", "playerTwo"];
-  this.currentPlayer = 0;
-  this.turnComplete = true;
-  this.territory = [];
-}
-
-Match.prototype.update = function() {
-  if (currentMatch.turnInProgress) {
-    let currentPlayer = currentMatch.getCurrentPlayer();
-    drawGridSquare(coordToCell(mouseX, mouseY), palette[currentPlayer]);
+////// PLAYER \\\\\\
+class Player {
+  constructor(cell, name, color) {
+    this.position = cell;
+    this.name = name;
+    this.color = color;
   }
-  else if (currentMatch.turnComplete){
-    let current_cell = coordToCell(mouseX, mouseY)
-    let currentPlayer = currentMatch.getCurrentPlayer();
-    let newCell = new Cell(current_cell.column, current_cell.row, palette[currentPlayer]);
-    this.addCellToTerritory(newCell);
-    currentMatch.turnComplete = false;
-    currentMatch.currentPlayer = (currentMatch.currentPlayer == 0) ? 1 : 0;
-  }
-  for (cell of this.territory){
-    drawGridSquare(cell, cell.color);
+  draw() {
+    drawPlayer(this.position, this.color)
   }
 }
 
-Match.prototype.getCurrentPlayer = function() {
-  return this.players[this.currentPlayer]
-}
+////// MATCH \\\\\\
+class Match {
+  constructor() {
+    this.players = [];
+    this.currentTurnIndex = 0;
+    this.currentPlayer;
+    this.turnComplete = true;
+    this.territory = [];
+  }
+  update() {
+    this.currentPlayer = this.players[this.currentTurnIndex % this.players.length];
 
+    if (this.turnInProgress) {
+      drawGridSquare(coordToCell(mouseX, mouseY), palette[this.currentPlayer]);
+    }
+    else if (this.turnComplete){
+      let current_cell = coordToCell(mouseX, mouseY)
+      let currentPlayer = this.CurrentPlayer;
+      let newCell = new Cell(current_cell.column, current_cell.row, palette[currentPlayer]);
+      this.addCellToTerritory(newCell);
+      this.turnComplete = false;
+      this.currentPlayer = (currentMatch.currentPlayer == 0) ? 1 : 0;
+    }
+  }
+  draw(){
+    for (let cell of this.territory){
+      drawGridSquare(cell, cell.color);
+    }
+  }
+  addCellToTerritory(cell) {
+    let existingCell = this.getCellfromTerritory(cell);
+    if (existingCell) {
+      this.territory[this.territory.indexOf(existingCell)] = cell
+    }
+    else{
+      this.territory.push(cell);
+    }
+  }
+  getCellfromTerritory(cell){
+    for (let currentCell of this.territory) {
+      if (cell.row == currentCell.row && cell.column == currentCell.column) {
+        return currentCell;
+      }
+    }
+    return false;
+  }
+}
+////// INPUT \\\\\\
 function mouseDown() {
   currentMatch.turnComplete = false;
   currentMatch.turnInProgress = true;
@@ -99,24 +115,8 @@ function mouseUp(){
   currentMatch.turnInProgress = false;
 }
 
-Match.prototype.addCellToTerritory = function(cell) {
-  let existingCell = this.getCellfromTerritory(cell);
-  if (existingCell) {
-    this.territory[this.territory.indexOf(existingCell)] = cell
-  }
-  else{
-    this.territory.push(cell);
-  }
-}
-Match.prototype.getCellfromTerritory = function(cell){
-  for (currentCell of this.territory) {
-    if (cell.row == currentCell.row && cell.column == currentCell.column) {
-      return currentCell;
-    }
-  }
-  return false;
-}
 
+////// SCALING \\\\\\
 function windowResized(){
   defineGridSize();
   createCanvas(BOARD_SIZE * gridSize, BOARD_SIZE * gridSize);
@@ -135,6 +135,12 @@ function defineGridSize(){
   sixteenthGridSize = gridSize / 16;
 }
 
+function coordToCell(x, y){
+  return {row: Math.floor(y / gridSize ), column: Math.floor(x / gridSize)};
+}
+
+//////  DRAWING \\\\\\
+
 function drawGrid(){
   for (let row = 0; row <= BOARD_SIZE; row++){
     for (let column = 0; column <= BOARD_SIZE; column ++){
@@ -151,7 +157,7 @@ function drawGridIntersection(x, y){
 
 function drawOutline() {
   noFill();
-  stroke(palette[currentMatch.getCurrentPlayer()]);
+  stroke(palette.neutral);
   strokeWeight(eighthGridSize);
   square(0,0, gridSize * BOARD_SIZE);
 }
@@ -170,14 +176,10 @@ function drawGridSquare(cell, color = 50){
   square(cell.column * gridSize, cell.row * gridSize, gridSize);
 }
 
-function drawPlayer(cell, color = palette.playerOne){
+function drawPlayer(cell, color){
   let top_left = { x: cell.row * gridSize, y: cell.column * gridSize};
   strokeWeight(sixteenthGridSize);
   stroke(palette.neutral);
   fill(color);
   quad(top_left.x + halfGridSize, top_left.y, top_left.x + gridSize, top_left.y + halfGridSize, top_left.x + halfGridSize, top_left.y + gridSize, top_left.x, top_left.y + halfGridSize);
-}
-
-function coordToCell(x, y){
-  return {row: Math.floor(y / gridSize ), column: Math.floor(x / gridSize)};
 }
